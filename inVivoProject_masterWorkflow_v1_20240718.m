889608acd5a53faddb80b78544190bf5436de644
anjapayne <anjaelisahiggins@gmail.com>
2024-07-18 09:30:54 -0700

    initializes repo; repo will be used to organize all code relevant to the in vivo paper

---
+++ inVivoProject_masterWorkflow_v1_20240718.m
@@ -0,0 +1,26 @@
+% Master workflow to reproduce all analysis and plots included in Anja
+% Payne's in vivo paper
+% Written by Anja Payne
+% Last modified: 07/18/2024
+
+% Issues to resolve:
+%   - Implement step 3: get linearized rate maps
+%   - Implement step 1
+%   - Implement step 2
+
+% Steps:
+%   1) Use opto-tagging to identify WT and KO neurons
+%   2) Get spiking metrics (i.e. firing rate and burst index)
+%   3) Get the linearized rate maps
+
+%% Step 3: Get the linearized rate maps
+% Settings: 
+% Input: 
+% Output: 
+
+
+
+
+
+
+
