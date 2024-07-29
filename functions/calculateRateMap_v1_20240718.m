e701a041e17dd70e4183f36d2fa529519ce8a9ed
anjapayne <anjaelisahiggins@gmail.com>
2024-07-29 14:23:57 -0700

    adds the step to calculate the rate map

--- functions/calculateRateMap_v1_20240718.m
+++ functions/calculateRateMap_v1_20240718.m
@@ -1,172 +1,127 @@
-function calculateRateMap_v1_20240718
-% Calculate the linearized rate maps across all trials for each cells
-% Written by Anja Payne
-% Last Modified: 07/18/2024
+function data = calculateRateMap_v1_20240718(data, settings)
+    % Calculate the linearized rate maps across all trials for each cells
+    % Written by Anja Payne
+    % Last Modified: 07/29/2024
 
-% Inputs:
+    % Inputs:
+    %   1) data: the matlab structure where the spikes (binned into 4cm
+    %      bins and separated into trials/direction) are stored
+    %   2) settings: the matlab settings file where the ______ thresholds 
+    %      are stored
+    
+    % Outputs:
+    %   1) data: the structure with the high-velocity spike times included
 
-% Outputs:
-
-% Steps:
-%   1) Load relevant data and define necessary pathways
-%   2) Loop through each animal and each cell. Center the x and y position 
-%      around 0 and convert coordinates to mm
-%   3) Exclude spikes that occur before the video, after the video, and
-%      during periods of low velocity. 
-%   4) Split the file into trials
-%   5) Linearize the track into bins and assign each spike to the nearest
-%      bin. 
-%   6) Calculate the rate maps for each trial
-%   7) Plot as a 2 dimensional heat map with time on the x-axis and trial
-%      number on the y-axis. 
-
-
-%% Step 1: %%%%
-clear
-clc
-% Define pathways
-%addpath(genpath('Z:\Anja\Matlab Code\CreateRateMaps')); 
-%addpath(genpath('Z:\Anja\Matlab Code\LoadFiles')); 
-%addpath(genpath('Z:\Anja\Matlab Code\GeoffCode\Organized Code'));
-animalFile = 'AP_AnimalData_Cohort13_Track'; 
-animalInfoFile = ['Z:\Anja\Data\In Vivo Data\AnimalInfo_ExcelFiles\', animalFile]; 
-animalInfo = excel2Mat(animalInfoFile)
-animalInfo_sessionSpecific = selectSessionType(animalInfo, 'Track'); 
-%%
-% Define parameters
-trackWidth = 52; % In cm
-trackLength = 80; % In cm
-close all; 
-%%%% Step 2: %%%%
-for iAnimal = 1%:length(animalInfo_sessionSpecific); 
-   for iCluster = 64%1:length(animalInfo_sessionSpecific(iAnimal).directory);
-       display(['Calculating for cluster ', num2str(iCluster) ' of animal ', num2str(iAnimal)]);
-       %%%% Step 1: %%%%
-        % Load the event file
-        event_file = [animalInfo_sessionSpecific(iAnimal).directory{iCluster}, '\Events.nev'];
-        % Load the spike data for the relevant cluster
-        load([animalInfo_sessionSpecific(iAnimal).directory{iCluster}, '\', animalInfo_sessionSpecific(iAnimal).clusters{iCluster}]);
-        % Load the video file
-        idcs = strfind(animalInfo_sessionSpecific(iAnimal).directory{iCluster}, '\');
-        parent_directory = animalInfo_sessionSpecific(iAnimal).directory{iCluster}(1:idcs(end)-1);
-        load([parent_directory, '\Position.mat']);
-        % Load the csc data
-        ephys_directory = animalInfo_sessionSpecific(iAnimal).directory{iCluster}; 
-        CSC_file = [ephys_directory, '\CSC1.ncs']; 
-        csc_data = readCSC(CSC_file); 
-        csc_timepoints_lowSampling = csc_data.ts; 
-        csc_samples = csc_data.samp; 
-        
-        %%%% Step 2: %%%%
-        % Exclude any position data that occurs before the video turns on. 
-        clipped_indices = find(aligned_pos(:,1) ~= -1000);
-        x_clipped = aligned_pos(clipped_indices, 2);
-        y_clipped = aligned_pos(clipped_indices, 1); 
-        t_clipped = aligned_pos_ts(clipped_indices); 
-        % Center the position data around the coordinates (0,0)
-        [x_pos_centered, y_pos_centered] = centerBox(x_clipped, y_clipped); 
-        % Convert the scale from pixels to cm
-        widthConversionFactor = trackWidth/(max(x_pos_centered) - min(x_pos_centered)); 
-        lengthConversionFactor = trackLength/(max(y_pos_centered) - min(y_pos_centered)); 
-        x_pos_cm = x_pos_centered*widthConversionFactor;
-        y_pos_cm = y_pos_centered*lengthConversionFactor; 
-        
-        %%%% Step 3: %%%%
-        % Interpolate the CSC timepoints so that they are at the same
-        % sampling rate as the CSC samples
-        csc_samples_points = linspace(1, length(csc_timepoints_lowSampling), length(csc_samples)); 
-        csc_timepoints_lowSampling_points = [1:length(csc_timepoints_lowSampling)]; 
-        csc_timepoints = interp1(csc_timepoints_lowSampling_points, csc_timepoints_lowSampling, csc_samples_points, 'linear'); 
-        csc_timepoints_msec = csc_timepoints/1000; 
-        ephys_t0 = csc_timepoints_msec(1); 
-        % Exclude any spikes that occur before or after the video has started. 
-        spike_time_msec = TS/10; 
-        %exclude_timepoints = csc_timepoints_msec(binaryVideoOn == 0); 
-        % Exclude any spikes that occur during periods of low velocity. 
-        [include_spikes, ave_vel, std_vel, vel_thresh, velocity] = excludeLowVelocity(x_pos_cm, y_pos_cm, t_clipped, spike_time_msec, ephys_t0); 
-
-        %%%% Step 4: %%%%
-        % Split the spikes and position into trials. 
-        [xTrials,yTrials,tTrials,sTrials] = splitFilesByTrials(include_spikes, x_pos_cm, y_pos_cm, t_clipped);
-
-        %%%% Step 5: %%%%
-        % Determine how you want the linearized trials organized. A value
-        % of 0 will organize the trials in time (1 to the end). A value of
-        % 1 will organize the trials into clockwise vs. counterclockwise. 
-        organization = 1;
-        % Linearize the track into bins and assign each spike to the
-        % nearest bin
-        trials = {};
-        [occupancyBinned, spikesBinned, trials] = linearizeData(xTrials, yTrials, tTrials, sTrials, organization); 
-%%
-        %%%% Step 6: %%%%
-        % Calculate the rate maps
-        map = []; timeMap = []; 
-        for iTrials = 1:length(spikesBinned); 
-            [map(iTrials,:), timeMap(iTrials,:), spikeMap(iTrials,:)] = calcRateMapLinearized(occupancyBinned{iTrials}, spikesBinned{iTrials});
+    % Steps:
+    %   1) Loop through genotypes, animals, and cells and calculate the 
+    %      rate maps for each trial
+    %   2) If the user has selected this option, plot the rate maps as a
+    %      two-dimensional heat map with time on the x-axis and trial
+    %      number on the y-axis. 
+    %   3) Ask the user if they want to save the newly generated data
+    
+    nBins = settings.rateMaps.trackSize / settings.rateMaps.binSize;
+    for iGenotype = 1%:length(fieldnames(data));
+        genotypes = fieldnames(data); 
+        genotypeData = data.(genotypes{iGenotype}); 
+        for iAnimal = 1%:length(genotypeData); 
+            if isempty(genotypeData{iAnimal}) == 1; 
+                continue
+            else
+                [~,n] = size(genotypeData{iAnimal});
+                for iCluster = 1%:n;
+                    %% Step 1: Get the rate maps
+                    display(['Calculating for cluster ', num2str(iCluster) ' of animal ', num2str(iAnimal)]);
+                    for iDir = 1:2;
+                        % Assign binned positions and spikes
+                        if iDir == 1; 
+                            posBinned = data.WT{iAnimal}(iCluster).binnedPosByTrial.cw;
+                            spikesBinned = data.WT{iAnimal}(iCluster).binnedSpikesByTrial.cw;
+                        elseif iDir == 2; 
+                            posBinned = data.WT{iAnimal}(iCluster).binnedPosByTrial.ccw;
+                            spikesBinned = data.WT{iAnimal}(iCluster).binnedSpikesByTrial.ccw;
+                        end
+                        map = []; timeMap = []; 
+                        for iTrials = 1:length(spikesBinned); 
+                            [map(iTrials,:), timeMap(iTrials,:), spikeMap(iTrials,:)] = calculateLinearizedRateMap(posBinned{iTrials}, spikesBinned{iTrials}, nBins);
+                        end
+                        % Shift the map so that the reward zone is at the end of the track
+                        map = circshift(map, -2, 2);
+                        % Multiple the map by the sampling rate to get
+                        % spikes per second
+                        map = map * settings.velocity.samplingRate; 
+                        
+                        % Append to data structure
+                        if iDir == 1; 
+                            data.(genotypes{iGenotype}){iAnimal}(iCluster).rateMap.cw = map;
+                            data.(genotypes{iGenotype}){iAnimal}(iCluster).timeMap.cw = timeMap;
+                        elseif iDir == 2; 
+                            data.(genotypes{iGenotype}){iAnimal}(iCluster).rateMap.ccw = map;
+                            data.(genotypes{iGenotype}){iAnimal}(iCluster).timeMap.ccw = timeMap;
+                        end
+                    end
+                end
+            end
         end
-        % Shift the map so that the reward zone is at the end of the track
-        %map = circshift(temp_map, -2, 2);
-        
-        %%
-        %%%% Step 7: %%%%
-        % Plot as a 2 dimensional heat map with time on the x-axis and trial
-        % number on the y-axis.
-        %map = map(1:length(trials)/2,:);
-        imAlpha = ones(size(map)); 
-        imAlpha(isnan(map)) = 0;
-        h1 = figure(1);
-        imagesc(map*16000, 'AlphaData', imAlpha);
-        set(gca, 'color', [0.8,0.8,0.8]); 
-        title(['Linearized Rate Map for ', animalInfo_sessionSpecific(iAnimal).cell_name{iCluster}], 'Interpreter', 'none');
-        set(gca, 'ytick', [1:length(trials)]); 
-        set(gca, 'YTickLabels', trials); 
-        xlabel('Bin');
-        ylabel('Trial'); 
-        colorbar;
-        %saveas(h1, ['Z:\Anja\Figures\4. Spatial Characteristics\LinearTrack\Linearized\Sparse\PotentialExamples\', animalInfo_sessionSpecific(iAnimal).cell_name{iCluster}, '_AverageLinearizedMap.tif'],'tif'); 
-        %saveas(h1, [parent_directory, '\', animalInfo_sessionSpecific(iAnimal).cell_name{iCluster}, '_BinSize4_LinearizedBurstRateMap.fig'],'fig');
-        %saveas(h1, ['Z:\Anja\Data\Cells\Rate Maps\Linearized Rate Maps\Linear Track\', animalInfo_sessionSpecific(iAnimal).cell_name{iCluster}, '_BinSize4_Linearized.tif'],'tif');       
-        %clf
-        % If desired, plot the average rate map
-        
-        h2 = figure(2);
-        h2 = imagesc(nanmean(map(1:length(trials)/2,:))*16000);
-        %saveas(h2, ['Z:\Anja\Figures\4. Spatial Characteristics\LinearTrack\Linearized\Dense\PotentialExamples\bestKOexamples\', animalInfo_sessionSpecific(iAnimal).cell_name{iCluster}, '_AverageLinearizedMap.tif'],'tif'); 
-        %%
-        
-        % Concatenate all rate maps into one variable
-        allRateMaps{iAnimal, iCluster} = map; 
-        % Concatenate all trials into one variable
-        allTrials{iAnimal, iCluster} = trials; 
-        % Concatenate all time maps into one variable
-        allTimeMaps{iAnimal, iCluster} = timeMap; 
-
-   end
+    end
+    
+    %% Step 2: Step 2: Save
+    saveFile_v1_20240718(data, settings, 'rateMaps') 
+    
 end
 
-% Save variables
-% idcs   = strfind(animalInfoFile,'\');
-% save_directory = animalInfoFile(idcs(end)+1:end);
-% save(['Z:\Anja\Data\In Vivo Data\AnalyzedData\', save_directory, 'Velocity2_BinSize4_LinearizedRateMaps'], 'allRateMaps', 'allTrials', 'allTimeMaps');
-%save(['Z:\Anja\Data\In Vivo Data\AnalyzedData\', save_directory, '_V2B4_Bursts'], 'allRateMaps', 'allTrials', 'allTimeMaps');
-%save(['Z:\Anja\Data\In Vivo Data\AnalyzedData\', save_directory, '_V2B4_Events'], 'allRateMaps', 'allTrials', 'allTimeMaps');
-%save(['Z:\Anja\Data\In Vivo Data\AnalyzedData\', save_directory, '_V2B4_Singles'], 'allRateMaps', 'allTrials', 'allTimeMaps');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% Helper Functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% Temporary plotting
-trial = 11; trial2 = 41; 
-close all; figure(1); subplot(1,2,1); hold on;
-plot(xTrials{trial}, yTrials{trial}, 'k'); 
-plot(xTrials{trial}(1:100000), yTrials{trial}(1:100000), 'g'); 
-[~, ~, sPoints] = intersect(sTrials{trial}, round(tTrials{trial}))
-scatter(xTrials{trial}(sPoints), yTrials{trial}(sPoints), '*r'); 
-subplot(1,2,2); 
-imagesc(map(trial2,:)); 
-%set(gca, 'ytick', [1:length(trials)]); 
-set(gca, 'YTickLabels', trials{trial2}); 
-set(gcf, 'position', [200, 200, 1000, 500]); 
-%%
-figure(1); clf; hold on;
-plot([0:4:264], nanmean(map(length(trials)/2:end,:))*16000);
-plot([1,264], [0.1*max(nanmean(map(length(trials)/2:end,:))*16000), 0.1*max(nanmean(map(length(trials)/2:end,:))*16000)]); 
-plot([1,264], [0.5*max(nanmean(map(length(trials)/2:end,:))*16000), 0.5*max(nanmean(map(length(trials)/2:end,:))*16000)]); 
+function [map, timeMap, spikeMap] = calculateLinearizedRateMap(pos, spikes, nBins)
+    % Gets the rate map for a linearized representation of the map
+    % Original code from Leutgeb lab with modifications by A. Payne
+    % Inputs:
+    %   1) pos: the binned positions
+    %   2) spikes: the binned spikes
+    % Outputs:
+    %   1) map: a heatmap of firing rate per bin
+    %   2) timeMap: a heatmap of time per bin
+    %   3) spikeMap: a heatmap of spike number per bin
+    
+    spikeMap = zeros(1, nBins); 
+    for iSpike = 1:length(spikes);
+        spikeMap(spikes(iSpike)) = spikeMap(spikes(iSpike)) + 1; 
+    end
+    timeMap = zeros(1, nBins); 
+    for iTime = 1:length(pos);
+       timeMap(pos(iTime)) = timeMap(pos(iTime)) + 1; 
+    end
+
+    % Set unoccupied pixels to NaN; 
+    minOccupancy = 1; 
+    timeMap(timeMap < minOccupancy) = NaN; 
+    rawMap = spikeMap./timeMap; 
+    % Shift the map so that the reward zone is at the end
+    rawMap = circshift(rawMap, -2, 2);
+    % Add some padding for smoothing to work effectively
+    rawMap = padarray(rawMap', 2, NaN); 
+    
+    % Smooth the rate map using the Gaussian filter as well as an adaptive
+    % smoothing technique
+    % Define the Gaussian smoothing that will be applied to each bin. 
+    box = [0.0200 0.1000 0.1600 0.1000 0.0200]; 
+    for i = 3:length(rawMap)-2; 
+        current_sum = 0;
+        current_box = 0;
+        box_i = 0;
+        for ii = i-2:i+2;
+          box_i = box_i + 1;
+          if ~isnan(rawMap(ii));
+             current_sum = current_sum + rawMap(ii) * box(box_i);
+             current_box = current_box + box(box_i); 
+          end
+        end
+        map(i) = current_sum/current_box; 
+    end
+    
+    map = map(3:end); 
+    
+end
+
--- inVivoProject_masterWorkflow_v1_20240718.m
+++ inVivoProject_masterWorkflow_v1_20240718.m
@@ -1,11 +1,11 @@
 % Master workflow to reproduce all analysis and plots included in Anja
 % Payne's in vivo paper
 % Written by Anja Payne
-% Last modified: 07/25/2024
+% Last modified: 07/29/2024
 
 % Issues to resolve/currently working on:
-%   - i think the processed data should be organized into folders...
-%   - run step 5 and save
+%   - rerun step 5 when you have an hour to kill
+%   - step 6: rate maps
 %   - step 8: get the in-field spikes
 
 % Steps:
@@ -81,7 +81,7 @@ filePaths = data;
 % Outputs: 
 spikeTimes = getSpikeTimes_v1_20240725(filePaths, filePathSettings); toc
 
-%% Step 5: Exclude spikes that occur during low velocity (takes  min)
+%% Step 5: Exclude spikes that occur during low velocity (takes ~50 min)
 clear;clc;tic;
 
 % Settings: 
@@ -90,8 +90,8 @@ filePath = getMostRecentFilePath_v1_20240723(fileNameBase);
 loadFileName = [fileNameBase, '_v', filePath{2}, filePath{3}(1:end-4)];
 load([filePath{1}, '\', loadFileName, '_settings.mat']);
 settings.velocity.threshold = 2; % velocity threshold = 2 cm/sec
+settings.velocity.samplingRate = 16000; % sampling rate of video recording
 settings.velocity.timeToAverage = 1; % time to average over in seconds
-stepSize = 16000 * settings.velocity.timeToAverage; % step size is determined by number of frames per second and the time to average over
 settings.rateMaps.trackWidth = 52; % cm; used to convert from pixels to cm
 settings.rateMaps.trackLength = 80; % cm; used to convert from pixels to cm
 settings.rateMaps.binSize = 4; % 4 cm bins
@@ -104,163 +104,25 @@ spikeTimes = data;
 % Outputs: 
 binnedSpikesByTrial = getHighVelocitySpikesByTrial_v1_20240725(spikeTimes, settings); toc
 
-
-
-
-
-%% Step 6: Split the spikes into trials
-clear;clc;
+%% Step 6: Get the linearized rate maps
+clear;clc;tic;
 
 % Settings: 
 fileNameBase = 'highVelocitySpikeTimes';
 filePath = getMostRecentFilePath_v1_20240723(fileNameBase);
 loadFileName = [fileNameBase, '_v', filePath{2}, filePath{3}(1:end-4)];
 load([filePath{1}, '\', loadFileName, '_settings.mat']);
-
-% Inputs: 
-loadFileName = [fileNameBase, '_v', filePath{2}, filePath{3}];
-load([filePath{1}, '\', loadFileName]);
-highVelocitySpikeTimes = data;
-%%
-% Outputs: 
-
-
-
-
-xValues = find(abs(allX)<10); 
-yValues1 = find(allY > -40);
-yValues2 = find(allY < -30);
-yValues = intersect(yValues1, yValues2);
-putativeTrialStart = intersect(xValues, yValues);
-
-%%% Step 2: %%%%
-trialJump = 50000; % ~3 seconds; used historically
-count = 1; 
-trialStart = NaN(1, 10000); 
-for itrialStart = 1:length(putativeTrialStart)-1; 
-    % Find the points where the animal's position 'jumps.' That is, when
-    % a new trial starts. 
-    tempJump = diff([putativeTrialStart(itrialStart), putativeTrialStart(itrialStart+1)]);
-    if tempJump > trialJump;
-        check(count) = tempJump;
-        trialStart(count) = putativeTrialStart(itrialStart); 
-        count = count +1; 
-    end
-end
-%{
-for iTrials = 1:length(trialStart); 
-    tempTrialY = allY(trialStart(iTrials):trialStart(iTrials+1)-1); 
-    % Find all the positions that occur on the sides of the track
-    sides = find(tempTrialY > -20 & tempTrialY < 20); 
-    % Find how many of those occur 
-end
-%}
-
-%%%% Step 3: %%%%
-% Save each trial in its own row of a matrix
-trialStart(isnan(trialStart)) = []; 
-count2 = 1; allTrialsY = {}; allTrialsX = {};
-for iTrials = 1:length(trialStart)-1; 
-    tempTrialY = allY(trialStart(iTrials):trialStart(iTrials+1)-1); 
-    % Make sure that the trial includes a timepoint where the animal runs
-    % across the back of the track
-    % Currently testing: also make it mandatory that the animal crosses
-    % both arms of the track only once
-    if sum(tempTrialY > 30) ~= 0;
-        allTrialsY{count2} = tempTrialY; 
-        tempTrialX = allX(trialStart(iTrials):trialStart(iTrials+1)-1);
-        allTrialsX{count2} = tempTrialX; 
-        tempTrialT = allT(trialStart(iTrials):trialStart(iTrials+1)-1); 
-        allTrialsT{count2} = tempTrialT; 
-        tempTrialSpikes = intersect(round(allSpikes), round(tempTrialT)); 
-        allTrialsSpikes{count2} = tempTrialSpikes;
-        count2 = count2 + 1;
-    end
-end
-
-
-
-
-
-%% Step 4: 
-clear;clc;
-
-% Settings: 
-fileNameBase = 'filePathsByGenotype';
-trackWidth = 52; trackLength = 80; % In cm
-track{1} = trackWidth; track{2} = trackLength; 
-
-% Inputs: 
-filePath = getMostRecentFilePath_v1_20240723(fileNameBase);
-loadFileName = [fileNameBase, '_v', filePath{2}, filePath{3}];
-load([filePath{1}, '\', loadFileName]);
-filePaths = data;
-
-% Outputs: 
-directoryPath = ''; 
-for iGenotype = 1%:length(fieldnames(data));
-    genotypes = fieldnames(data); 
-    genotypeData = data.(genotypes{iGenotype}); 
-    for iAnimal = 1%:length(genotypeData); 
-        for iCluster = 1:2%:length(genotypeData{iAnimal});
-            display(['Calculating for cluster ', num2str(iCluster) ' of animal ', num2str(iAnimal)]);
-            %% Step 1: Load the data
-            % If this data has been loaded in a previous iteration,
-            % skip loading it now
-            newDirectoryPath = genotypeData{iAnimal}(iCluster).directory;
-            if strcmp(directoryPath, newDirectoryPath) == 1;
-                % Do nothing
-            elseif strcmp(directoryPath, newDirectoryPath) == 0;
-                directoryPath = newDirectoryPath; 
-                % Load the position information
-                idcs = strfind(directoryPath{1}, '\');
-                parent_directory = directoryPath{1}(1:idcs(end)-1);
-                load([parent_directory, '\Position.mat']);
-                % Load the CSC data
-                CSC_file = [directoryPath{1}, '\CSC1.ncs']; 
-                csc_data = readCSC(CSC_file); 
-                ephys_t0 = csc_data.ts(1); 
-            end
-            % Load the spike data for the relevant cluster
-            spikeDataPath = strcat(directoryPath, '\', genotypeData{iAnimal}(iCluster).fileName, '.mat');
-            load(spikeDataPath{1});
-            spike_time_msec = TS/10;
-            
-            x_pos = aligned_pos(:,1); 
-            y_pos = aligned_pos(:,2);
-            widthConversionFactor = track{1}/(max(x_pos) - min(x_pos)); 
-            lengthConversionFactor = track{2}/(max(y_pos) - min(y_pos)); 
-            x_pos_cm = x_pos.*widthConversionFactor;
-            y_pos_cm = y_pos.*lengthConversionFactor;
-                
-            %% Step 2: Exclude spikes during low velocity
-            [include_spikes, ave_vel, std_vel, vel_thresh, velocity] = excludeLowVelocity(x_pos_cm, y_pos_cm, t_clipped, spike_time_msec, ephys_t0); 
-
-                
-        end
-    end
-end
-
-%% Step 5: Get the linearized rate maps
-clear; clc;
-
-% Settings: 
-fileNameBase = 'filePathsByGenotype';
-trackWidth = 52; trackLength = 80; % In cm
-track{1} = trackWidth; track{2} = trackLength; 
-%generatePlots = 0; % set to 0 if you don't want the plots and 1 if you do
+settings.rateMaps.trackSize = 264; 
 
 % Input: 
-filePath = getMostRecentFilePath_v1_20240723(fileNameBase);
 loadFileName = [fileNameBase, '_v', filePath{2}, filePath{3}];
 load([filePath{1}, '\', loadFileName]);
-filePaths = data;
+binnedSpikesByTrial = data;
 
 % Output: 
-clc;
-calculateRateMap_v1_20240718(filePaths, track); 
+rateMaps = calculateRateMap_v1_20240718(binnedSpikesByTrial, settings); 
 
-%% Step 6: Get spatial metrics and field barcode
+%% Step 7: Get spatial metrics and associated barcode
 clear; clc; 
 
 % Settings: 
@@ -280,14 +142,57 @@ rateMapStructure = data;
 data = getSpatialMetrics_v1_20240724(rateMapStructure, settings); 
 
 %% Step 7: Get the in-field spikes for each field
-clear; clc; 
+clear;clc;tic;
 
 % Settings: 
-fileNameBase = 'rateMaps';
+fileNameBase = 'highVelocitySpikeTimes';
 filePath = getMostRecentFilePath_v1_20240723(fileNameBase);
 loadFileName = [fileNameBase, '_v', filePath{2}, filePath{3}(1:end-4)];
 load([filePath{1}, '\', loadFileName, '_settings.mat']);
-rateMapSettings = settings; 
-settings.rateMap.lowThresh = 0.1; 
-settings.rateMap.highThresh = 0.5; 
+
+% Inputs: 
+loadFileName = [fileNameBase, '_v', filePath{2}, filePath{3}];
+load([filePath{1}, '\', loadFileName]);
+binnedSpikes = data;
+
+% Outputs: 
+
+%% Step 9: Assign spikes and position to in-field
+% Pre-allocate based on the length of the data
+% if length(cw_spike_bins) > length(ccw_spike_bins); 
+%     spike_logical{iAnimal}{iCluster} = zeros(length(cw_spike_bins),2);
+%     allSpikes{iAnimal}{iCluster} = NaN(length(ccw_spike_bins),2);             
+% elseif length(ccw_spike_bins) > length(cw_spike_bins); 
+%     spike_logical{iAnimal}{iCluster} = zeros(length(ccw_spike_bins),2);
+%     allSpikes{iAnimal}{iCluster} = NaN(length(ccw_spike_bins),2);             
+% end
+% if length(cw_pos) > length(ccw_pos); 
+%     pos_logical{iAnimal}{iCluster} = zeros(length(cw_pos),2);
+% elseif length(ccw_pos) > length(cw_pos); 
+%     pos_logical{iAnimal}{iCluster} = zeros(length(ccw_pos),2);
+% end
+for iDir = 1:2; 
+    %allSpikes{iAnimal}{iCluster}{iDir} = {}; 
+    for iField = 1:max(barcode{iAnimal}{iCluster, iDir}); 
+        iBinField = find(barcode{iAnimal}{iCluster, iDir} == iField); 
+        if iDir == 1; 
+            % Determine which positions occur in the 'in-field' bin
+            binIndex_pos = ismember(cw_pos, iBinField); 
+            % Determine which spikes occur in the 'in-field' bin
+            binIndex_spikes = ismember(cw_spike_bins, iBinField); 
+            allSpikes{iAnimal}{iCluster}(1:length(cw_spikes),iDir) = cw_spikes;                            
+        elseif iDir == 2; 
+            % Determine which positions occur in the 'in-field' bin                    
+            binIndex_pos = ismember(ccw_pos, iBinField); 
+            % Determine which spikes occur in the 'in-field' bin
+            binIndex_spikes = ismember(ccw_spike_bins, iBinField);  
+            allSpikes{iAnimal}{iCluster}(1:length(ccw_spikes),iDir) = ccw_spikes;                                                             
+        end
+        [m,~] = size(allSpikes{iAnimal}{iCluster});
+        pos_logical{iAnimal}{iCluster}(binIndex_pos, iDir) = iField; 
+        spike_logical{iAnimal}{iCluster}(1:m, iDir) = 0;  
+        spike_logical{iAnimal}{iCluster}(binIndex_spikes, iDir) = iField;  
+    end
+end  
+
 
